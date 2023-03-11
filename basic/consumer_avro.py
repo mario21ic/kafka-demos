adc8485fa9c995bc8e4c3b7ba42a91b38d05c697
Mario IC <mario21ic@gmail.com>
2023-03-10 21:45:39 -0500

    feat(global) updating avro examples

--- basic/consumer_avro.py
+++ basic/consumer_avro.py
@@ -25,12 +25,12 @@ for message in consumer:
     #print(msg)
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
-    #print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-    #                                      message.offset, message.key,
-    #                                      message.value))
     bytes_reader = io.BytesIO(message.value)
     decoder = BinaryDecoder(bytes_reader)
     reader = DatumReader(schema)
-    user1 = reader.read(decoder)
-    print('user:', user1)
+    value = reader.read(decoder)
+    #print('user:', user1)
+    print("topic=%s partition=%d offset=%d key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          value))
 
